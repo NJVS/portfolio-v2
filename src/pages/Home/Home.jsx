7db395e2af8184c2b57fdfd4b66972b7982e7f4e
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-07-18 15:07:45 +0800

    home page quote section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import Hero from './components/Hero/Hero';
+import Quote from './components/Quote/Quote';
+
 import styles from './Home.module.scss';
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      
+      <Hero />
+      <Quote />
     </div>
   )
 }
---
+++ src/pages/Home/components/Quote/Quote.jsx
@@ -0,0 +1,25 @@
+import styles from './Quote.module.scss';
+import quoteIcon from 'assets/icons/quotes.svg';
+import dots from 'assets/illustrations/dot-single-sm.png';
+import rect from  'assets/illustrations/rect-single-sm.png';
+
+const Quote = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h4>Luck is what happens when preparation meets oportunity</h4>
+          <img src={quoteIcon} className={styles.quote_top} />
+          <img src={quoteIcon} className={styles.quote_bot} />
+          <div className={styles.author}>
+            <p>- Seneca</p>
+          </div>
+        </div>
+        <div className={styles.illustration_left}><img src={dots} /></div>
+        <div className={styles.illustration_right}><img src={rect} /></div>
+      </div>
+    </section>
+  )
+}
+
+export default Quote
