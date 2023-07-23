d5d78a6b39e550a6369a8b24a78b356480d318a2
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-07-23 22:31:16 +0800

    footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,36 @@
 import styles from './Footer.module.scss';
+import { ReactComponent as Logo } from 'assets/icons/Logo-solid.svg';
+import { ReactComponent as IconGithub } from 'assets/icons/github.svg';
+import { ReactComponent as IconLinkedin } from 'assets/icons/linkedin.svg';
+import { ReactComponent as Iconfb } from 'assets/icons/fb-messenger.svg';
+import { ReactComponent as Icontwitter } from 'assets/icons/twitter.svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.logo}>
+            <Logo />
+            <h4>Neil</h4>
+          </div>
+          <a href="mailto:neiljonathanv.sagisi@gmail.com" target='_blank'>
+            neiljonathanv.sagisi@gmail.com
+          </a>
+          <p>front-end developer and web designer</p>
+        </div>
+        <div className={styles.socmed}>
+          <h4>Media</h4>
+          <ul>
+            <li><a href="" target='_blank'><IconGithub /></a></li>
+            <li><a href="" target='_blank'><IconLinkedin /></a></li>
+            <li><a href="" target='_blank'><Iconfb /></a></li>
+            <li><a href="" target='_blank'><Icontwitter /></a></li>
+          </ul>
+        </div>
+        <p className={styles.attribution}>Copyright &copy;2023. Made by Neil</p>
+      </div>
+    </footer>
   )
 }
 
