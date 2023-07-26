6140cdfab12eef1852b5a476fa767111ce514cf3
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-07-26 22:09:48 +0800

    page header

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,8 +1,13 @@
 import styles from './PageHeader.module.scss';
 
-const PageHeader = () => {
+const PageHeader = ({title, desc}) => {
   return (
-    <div>PageHeader</div>
+    <header className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1><span>/</span>{title}</h1>
+        <p>{desc}</p>
+      </div>
+    </header>
   )
 }
 
