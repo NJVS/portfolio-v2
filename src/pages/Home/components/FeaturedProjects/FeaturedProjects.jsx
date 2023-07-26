f2f76744ef23a2dd8fa6b1b156c490da3e86a3ae
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-07-26 22:11:50 +0800

    update styles

--- src/pages/Home/components/FeaturedProjects/FeaturedProjects.jsx
+++ src/pages/Home/components/FeaturedProjects/FeaturedProjects.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import projects from 'data/projects.json';
 import styles from './FeaturedProjects.module.scss';
 import { ReactComponent as Arrow } from 'assets/icons/arrow-right.svg';
-import CardProject from 'components/CardProject/CardProject';
+import ProjectShowcase from 'components/ProjectShowcase/ProjectShowcase';
 import dotSingle from 'assets/illustrations/dot-single.png';
 import reqSingle from 'assets/illustrations/rect-single.png';
 
@@ -16,17 +16,7 @@ const FeaturedProjects = () => {
           <Link to="/projects">View More <Arrow /></Link>
         </div>
         <div className={styles.body}>
-          {projects.featured.map((item, index) =>
-            <CardProject
-              key={index}
-              thumbnail={item.thumbnail}
-              stacks={item.stacks.join(' ')}
-              title={item.title}
-              desc={item.desc}
-              demoUrl={item.liveUrl}
-              repoUrl={item.repoUrl}
-            />
-          )}
+          <ProjectShowcase projects={projects.featured} />
         </div>
         <div className={styles.illustration_left}>
           <img src={reqSingle} alt="" />
