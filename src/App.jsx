5c198e0bf8106e878cef67585ed64db225da212c
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-07-26 22:13:37 +0800

    projects page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "components/Navbar/Navbar";
 import Home from "pages/Home/Home";
+import Projects from "pages/Projects/Projects";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </>
   );
