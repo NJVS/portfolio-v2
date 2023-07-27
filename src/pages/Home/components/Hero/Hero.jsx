48dfa8ff1faefe04d5b64ecc3ee958696aead60e
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-07-27 12:53:34 +0800

    scroll arrow

--- src/pages/Home/components/Hero/Hero.jsx
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -1,15 +1,25 @@
 import styles from './Hero.module.scss';
+import useWindowDimensions from 'hooks/useWindowDimensions';
 import { ReactComponent as Arrow } from 'assets/icons/arrows.svg';
 import rectdouble from 'assets/illustrations/rect-double.png';
 import dotrect from 'assets/illustrations/dot-rect.png';
 
 const Hero = () => {
+  const { height } = useWindowDimensions();
+
+  function scrollToHandler() {
+    window.scroll({
+      top: height,
+      behavior: 'smooth'
+    })
+  }
+
   return (
     <section className={styles.container}>
       <div className={styles.wrapper}>
         <h1>Neil is a <span>web designer</span> and <span>fron-end developer</span></h1>
         <p>He create responsive website where technology meets creativity</p>
-        <button className={styles.nav_arrow}><Arrow /></button>
+        <button className={styles.nav_arrow} onClick={scrollToHandler}><Arrow /></button>
         <div className={styles.illustration_left}>
           <img src={rectdouble} alt="double rect" />
         </div>
