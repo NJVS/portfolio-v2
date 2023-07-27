ea569d646b37e9bf391c0304d3c77ed57cd5d24a
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-07-27 12:40:03 +0800

    mailto link &  side illustration

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -3,6 +3,8 @@ import styles from './Contacts.module.scss';
 import PageHeader from 'components/PageHeader/PageHeader';
 import Footer from 'components/Footer/Footer';
 import { ReactComponent as MailIcon } from 'assets/icons/email.svg';
+import dotRect from 'assets/illustrations/dot-rect.png';
+import rectDouble from 'assets/illustrations/rect-double.png';
 
 const Contacts = () => {
   return (
@@ -15,10 +17,16 @@ const Contacts = () => {
         </p>
         <div className={styles.contact_info}>
           <h5>Message me here</h5>
-          <Link>
+          <Link to="mailto:neiljonathanv.sagisi@gmail.com" target='_blank'>
             <MailIcon />
             neiljonathanv.sagisi@gmail.com
           </Link>
+        </div>
+        <div className={styles.illustration_left}>
+          <img src={dotRect} alt="" />
+        </div>
+        <div className={styles.illustration_right}>
+          <img src={rectDouble} alt="" />
         </div>
       </div>
       <Footer />
