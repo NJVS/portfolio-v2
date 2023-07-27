eb4ff5cb44443986597dc06cd529da1e015db2c4
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-07-27 12:45:53 +0800

    target props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,14 @@ import { forwardRef } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
 
-const Button = ({ type, url, className, onClick, children }, ref) => {
+const Button = ({ type, url, target, className, onClick, children }, ref) => {
   return type === 'link' ? (
-    <Link ref={ref} to={url} target="_blank" className={`${styles.container} ${className ? className : ''}`}>
+    <Link
+      ref={ref}
+      to={url}
+      className={`${styles.container} ${className ? className : ''}`}
+      target={`${target ? target : ''}`}
+    >
       {children}
     </Link>
   ) : (
