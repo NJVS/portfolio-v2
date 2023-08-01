80a7ee822d6a062ba31ee051235746c4ea1a341f
Neil Jonathan Sagisi <neiljonathanv.sagisi+github@gmail.com>
2023-08-01 19:26:17 +0800

    scroll top

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "components/Navbar/Navbar";
 import Home from "pages/Home/Home";
 import Projects from "pages/Projects/Projects";
 import About from "pages/About/About";
 import Contacts from "pages/Contacts/Contacts";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Navbar />
